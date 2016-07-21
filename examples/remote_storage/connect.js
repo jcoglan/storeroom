6238ac5c2824cede2589ee3bac9228ab4b43c4dd
James Coglan <jcoglan@gmail.com>
2016-07-21 20:35:34 +0100

    Extract the generic parts of the RemoteStorage demo into library code for connecting and authorizing with a server.

--- examples/remote_storage/connect.js
+++ examples/remote_storage/connect.js
@@ -1,25 +1,15 @@
-var oauth      = require('remotestorage-oauth'),
-    discover   = require('../../lib/adapters/remote_storage/discover'),
+var vstore     = require('../../'),
     localStore = require('./local_store');
 
 var address = 'jcoglan@5apps.com';
 
-var session = discover(address).then(function(response) {
-  var endpoint = response.authDialog,
-      client   = 'Demo',
-      scopes   = ['*:rw'],
-      options  = {browser: 'elinks', inline: true};
-
-  return new Promise(function(resolve, reject) {
-    oauth.authorize(endpoint, client, scopes, options, function(error, token) {
-      if (error)
-        reject(error);
-      else
-        resolve({address: address, webfinger: response, authorization: token});
-    });
-  });
+var session = vstore.connectRemoteStorage({
+  address: address,
+  client:  'Demo',
+  scopes:  ['*:rw'],
+  options: {browser: 'elinks', inline: true}
 });
 
 session.then(function(credentials) {
-  return localStore.put('/remote-storage/' + address, credentials);
-}).then(process.exit);
+  localStore.put('/remote-storage/' + address, credentials);
+});
