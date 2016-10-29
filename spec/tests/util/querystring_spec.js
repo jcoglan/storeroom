547424b5541593db34c4e0f3c48eb96a5c9468a6
James Coglan <jcoglan@gmail.com>
2016-10-29 06:49:09 +0100

    Implement a function that works like the standard Object.assign(), and use it instead of extend().

--- spec/tests/util/querystring_spec.js
+++ spec/tests/util/querystring_spec.js
@@ -1,6 +1,6 @@
-var qs      = require("../../../lib/util/querystring"),
-    extend_ = require("../../../lib/util/extend"),
-    jstest  = require("jstest").Test
+var qs     = require("../../../lib/util/querystring"),
+    assign = require("../../../lib/util/assign"),
+    jstest = require("jstest").Test
 
 jstest.describe("querystring", function() { with(this) {
   it("serialises data", function() { with(this) {
@@ -10,6 +10,6 @@ jstest.describe("querystring", function() { with(this) {
 
   it("parses a string", function() { with(this) {
     assertEqual( {"search term": "black & white", n: "16"},
-                 extend_({}, qs.parse("search%20term=black%20%26%20white&n=16")) )
+                 assign({}, qs.parse("search%20term=black%20%26%20white&n=16")) )
   }})
 }})
